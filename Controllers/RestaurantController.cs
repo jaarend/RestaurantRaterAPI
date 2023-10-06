93dfe07857aae64ca3e5cc05c3bf8869d8b34103
Jayson Arend <jayson.arend@gmail.com>
2023-10-06 16:04:22 -0400

    created restaurant GET endpoints

--- Controllers/RestaurantController.cs
+++ Controllers/RestaurantController.cs
@@ -1,5 +1,8 @@
+using Microsoft.AspNetCore.Http.HttpResults;
 using Microsoft.AspNetCore.Mvc;
 using RestaurantRaterAPI.Data;
+using Microsoft.EntityFrameworkCore;
+using RestaurantRaterApi.Data;
 
 namespace RestaurantRaterAPI.Controllers;
 
@@ -11,5 +14,24 @@ public class RestaurantController : ControllerBase
     public RestaurantController(RestaurantDbContext context)
     {
         _context = context;
+    }
+
+    //Async GET Endpoint
+    public async Task<IActionResult> GetRestaurants()
+    {
+
+        List<Restaurant> restaurants = await _context.Restaurants.ToListAsync();
+        return Ok(restaurants);
+    }
+    [HttpGet("{id:int}")]
+    public async Task<IActionResult> GetRestaurantById(int id)
+    {
+        Restaurant? restaurant = await _context.Restaurants.FindAsync(id);
+        if (restaurant is null)
+        {
+            return NotFound();
+        }
+
+        return Ok(restaurant);
     }
 }
