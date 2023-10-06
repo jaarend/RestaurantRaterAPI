6ce99b6a43f64173240eb2469295b202d35732a0
Jayson Arend <jayson.arend@gmail.com>
2023-10-06 15:30:27 -0400

    initial commit and data

---
+++ Data/Restaurant.cs
@@ -0,0 +1,15 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace RestaurantRaterApi.Data;
+
+public class Restaurant
+{
+    [Key] //Primary Key
+    public int Id { get; set; }
+
+    [Required] //NOT NULL
+    [MaxLength(100)] //NVARCHAR (100)
+    public string Name { get; set; } = string.Empty
+    [Required, MaxLength(100)]
+    public string Location { get; set; } = string.Empty
+}
